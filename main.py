b483aef676d4536fa7af0da56d370cc18b3b7df2
本田真久 <hondamasahisa@hondamasahisanoMacBook-Pro.local>
2020-03-12 19:49:42 +0900

    main.pyを編集

--- main.py
+++ main.py
@@ -13,14 +13,20 @@ count = 0
 
 #空行列を作成
 date_data = []
-rate_data = []
+USDrate_data = []
+EURrate_data = []
+GBPrate_data = []
+
+
     
 ##データリストを作成
 for i in range(50):
     #########計測開始###########
     start = time.time() 
     date_data.append(str(datetime.datetime.now())) #計測時間を取得
-    rate_data.append(i) #計測データを取得
+    USDrate_data.append(i) #計測データを取得
+    EURrate_data.append(i) #計測データを取得
+    GBPrate_data.append(i) #計測データを取得
     end =time.time()
     time.sleep(sampling_interval-(end-start)) #一回のループが0.2秒になるように調整
     #########計測終了###########
@@ -39,11 +45,14 @@ for k in filename_list:
         #一つのファイルに5個記載
         for n in range(5):
             writer = csv.writer(f)
-            writer.writerow([date_data[count+n],rate_data[count+n]])
+            writer.writerow([date_data[count+n],USDrate_data[count+n],\
+            EURrate_data[count+n],GBPrate_data[count+n]])
     f.close()
     count = count+ 5
 
-new_data = copy.copy(rate_data) #配列をコピー
+USDnew_data = copy.copy(USDrate_data) #配列をコピー
+EURnew_data = copy.copy(EURrate_data) #配列をコピー
+GBPnew_data = copy.copy(GBPrate_data) #配列をコピー
 new_date = copy.copy(date_data) #配列をコピー
 
 end =time.time()
@@ -52,17 +61,21 @@ time.sleep(sampling_interval-(end-start))
 
 
 ##新規データを取得
-new_data.append(1) ##新規データ
-del new_data[0]
+USDnew_data.append(1) ##新規データ
+del USDnew_data[0]
 new_date.append(str(datetime.datetime.now()))
 del new_date[0]
     
-before = np.array(rate_data)
-after = np.array(new_data)
+before = np.array(USDrate_data)
+after = np.array(USDnew_data)
 
 delta_array = after - before
+###############################メイン処理################################
+#FFT
+#度数分布表
+
 print(before)
 print(after)
 print(delta_array)
-print(len(delta_array))
-            
+print(len(delta_array))            
+#######################################################################
